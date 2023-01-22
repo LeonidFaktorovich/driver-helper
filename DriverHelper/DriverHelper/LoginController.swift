14a12fdc71b2b0f99ab6350d845bee36b32df244
Leonid_Faktorovich <lefaktorovich@edu.hse.ru>
2023-01-22 19:49:10 +0300

    add login storyboard

---
+++ DriverHelper/DriverHelper/LoginController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginController.swift
+//  DriverHelper
+//
+//  Created by Леонид Факторович on 22.01.2023.
+//
+
+import Foundation
+import UIKit
+
+class LoginController : UIViewController {
+    
+    @IBOutlet weak var login_label: UILabel!
+    @IBOutlet weak var password_label: UILabel!
+    @IBOutlet weak var login_text: UITextField!
+    @IBOutlet weak var password_text: UITextField!
+    @IBOutlet weak var sign_button: UIButton!
+    override func loadView() {
+        super.loadView()
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ DriverHelper/DriverHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DriverHelper
+//
+//  Created by Леонид Факторович on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
