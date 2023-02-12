d2bbd3f9da4b9cf3d395174cab8972c0fac8b282
Leonid_Faktorovich <lefaktorovich@edu.hse.ru>
2023-02-12 16:39:24 +0300

    fix many same storyboards bug

--- DriverHelper/DriverHelper/MapController.swift
+++ DriverHelper/DriverHelper/MapController.swift
@@ -3,6 +3,16 @@ import UIKit
 import MapKit
 import Contacts
 
+var routes: Set<Route> = Set<Route>()
+
+let routes_lock = NSLock()
+
+
+var routes_to_draw: Set<Route> = Set<Route>()
+
+let routes_to_draw_lock = NSLock()
+
+
 class MapController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
@@ -11,33 +21,69 @@ class MapController: UIViewController, MKMapViewDelegate {
     let regionRadius: CLLocationDistance = 100000
     
     
-    let self_name = "Alina"
-    let self_color: UIColor = .systemPink
+    var self_name: String = ""
+    var self_color: UIColor = .systemRed
     
     var lastPressIsStart = false
     var lastStartCoordinate = CLLocationCoordinate2D()
     
+    let update_lock = NSLock()
+    var need_to_update = false
+    
     override func loadView() {
+        update_lock.lock()
+        
+        need_to_update = true
+        
+        update_lock.unlock()
+        
         super.loadView()
+        self_name = GetLogin()!
+        self_color = ColorFromName(name: self_name)
         let net_routes = main_user!.GetMap()
         for net_route in net_routes {
-            let route = Route(name: net_route.owner, startCoordinate: net_route.start, finishCoordinate: net_route.finish, initialColor: .green)
+            let route = Route(name: net_route.owner, startCoordinate: net_route.start, finishCoordinate: net_route.finish, initialColor: ColorFromName(name: net_route.owner))
+            
+            routes_lock.lock()
+            routes_to_draw_lock.lock()
+            
             if !routes.contains(route) {
                 routes_to_draw.insert(route)
             }
+            
+            routes_to_draw_lock.unlock()
+            routes_lock.unlock()
         }
         DrawRoutes()
         AddTapRecognizer()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 15.0) //- выполнить через 15 сек.
-        { [self] in
-           UpdateMap()
-        }
-        self.mapView.removeOverlays(self.mapView.overlays)
-
+        RepeatUpdateMap()
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        update_lock.lock()
+        
+        if !need_to_update {
+            need_to_update = true
+            update_lock.unlock()
+            RepeatUpdateMap()
+        } else {
+            need_to_update = true
+            update_lock.unlock()
+        }
+        
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        update_lock.lock()
+        
+        need_to_update = false
+        
+        update_lock.unlock()
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -87,8 +133,14 @@ class MapController: UIViewController, MKMapViewDelegate {
                 }
             }
         }
+        routes_lock.lock()
+        routes_to_draw_lock.lock()
+        
         routes.formUnion(routes_to_draw)
         routes_to_draw.removeAll()
+        
+        routes_lock.unlock()
+        routes_to_draw_lock.unlock()
     }
     
     @IBAction func tapMap(_ sender: UITapGestureRecognizer) {
@@ -105,7 +157,13 @@ class MapController: UIViewController, MKMapViewDelegate {
                 
                 // date - current time
                 let net_route = NetworkRoute(start: lastStartCoordinate, finish: tappedCoordinate, owner: self_name, date: Date())
+                
+                routes_lock.lock()
+                
                 routes.insert(Route(name: self_name, startCoordinate: lastStartCoordinate, finishCoordinate: tappedCoordinate, initialColor: self_color))
+                
+                routes_lock.unlock()
+                
                 main_user?.AddRoute(route: net_route)
                 let direction = MKDirections(request: request)
                 
@@ -153,19 +211,72 @@ class MapController: UIViewController, MKMapViewDelegate {
         tapRecognizer.require(toFail: doubleTapRecognizer)
     }
     func UpdateMap() {
+        update_lock.lock()
+        if !need_to_update {
+            update_lock.unlock()
+            return
+        }
+        update_lock.unlock()
+        
         let net_routes = main_user!.GetMap()
         for net_route in net_routes {
-            let route = Route(name: net_route.owner, startCoordinate: net_route.start, finishCoordinate: net_route.finish, initialColor: .green)
+            let route = Route(name: net_route.owner, startCoordinate: net_route.start, finishCoordinate: net_route.finish, initialColor: ColorFromName(name: net_route.owner))
+            
+            routes_lock.lock()
+            routes_to_draw_lock.lock()
+            
             if !routes.contains(route) {
                 routes_to_draw.insert(route)
             }
+            
+            routes_to_draw_lock.unlock()
+            routes_lock.unlock()
         }
         DrawRoutes()
+        RepeatUpdateMap()
+    }
+    func RepeatUpdateMap() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 15.0) //- выполнить через 15 сек.
         { [self] in
            UpdateMap()
         }
-
+    }
+    func ColorFromName(name: String) -> UIColor {
+        let name_to_color_table: [Character: UIColor] = [
+            "a": UIColor(red: 4/255, green: 59/255, blue: 92/255, alpha: 1),
+            "b": UIColor(red: 40/255, green: 67/255, blue: 135/255, alpha: 1),
+            "c": UIColor(red: 205/255, green: 209/255, blue: 228/255, alpha: 1),
+            "d": UIColor(red: 15/255, green: 10/255, blue: 222/255, alpha: 1),
+            "e": UIColor(red: 82/255, green: 78/255, blue: 183/255, alpha: 1),
+            "f": UIColor(red: 52/255, green: 45/255, blue: 113/255, alpha: 1),
+            "g": UIColor(red: 1/255, green: 1/255, blue: 122/255, alpha: 1),
+            "h": UIColor(red: 37/255, green: 41/255, blue: 88/255, alpha: 1),
+            "i": UIColor.systemRed,
+            "j": UIColor.systemPink,
+            "k": UIColor.systemBlue,
+            "l": UIColor.systemCyan,
+            "m": UIColor.systemPink,
+            "n": UIColor.systemTeal,
+            "o": UIColor.systemMint,
+            "p": UIColor.systemIndigo,
+            "q": UIColor.systemOrange,
+            "r": UIColor.systemPurple,
+            "s": UIColor.systemGreen,
+            "t": UIColor.red,
+            "u": UIColor(red: 1/255, green: 1/255, blue: 122/255, alpha: 1),
+            "v": UIColor.systemIndigo,
+            "w": UIColor.systemOrange,
+            "x": UIColor.systemRed,
+            "y": UIColor(red: 1/255, green: 1/255, blue: 122/255, alpha: 1),
+            "z": UIColor.systemGreen
+        ]
+        let lower_name = name.lowercased()
+        let first_character: Character = lower_name[lower_name.startIndex]
+        
+        if name_to_color_table[first_character] != nil {
+            return name_to_color_table[first_character]!
+        }
+        return UIColor.systemGreen
     }
 }
 
@@ -216,5 +327,4 @@ class Route : Hashable {
 //var routes: [Route] = [Route(name: "Alina", startCoordinate:  CLLocationCoordinate2D(latitude: 55.1, longitude: 37.2), finishCoordinate: CLLocationCoordinate2D(latitude: 55.5, longitude: 37.3), initialColor: .systemPink),
 //                       Route(name: "Maria", startCoordinate: CLLocationCoordinate2D(latitude: 55, longitude: 37), finishCoordinate: CLLocationCoordinate2D(latitude: 56, longitude: 38), initialColor: .purple),
 //                       Route(name: "Leonid", startCoordinate: CLLocationCoordinate2D(latitude: 55.8, longitude: 37.6), finishCoordinate:  CLLocationCoordinate2D(latitude: 55.1, longitude: 37.4), initialColor: .systemBlue)]
-var routes: Set<Route> = Set<Route>()
-var routes_to_draw: Set<Route> = Set<Route>()
+
--- DriverHelper/DriverHelper/SettingsController.swift
+++ DriverHelper/DriverHelper/SettingsController.swift
@@ -13,12 +13,25 @@ class SettingsController: UIViewController {
 
     @IBAction func LogOut(_ sender: Any) {
         UserExit()
+        
+        routes_lock.lock()
+        routes_to_draw_lock.lock()
+        
         routes.removeAll()
         routes_to_draw.removeAll()
-        let reg_storyboard = UIStoryboard(name: "Login", bundle: nil)
-        let reg_controller = reg_storyboard.instantiateViewController(withIdentifier: "Login") as! LoginController
-        reg_controller.modalPresentationStyle = .fullScreen
-        self.present(reg_controller, animated: true)
+        
+        routes_lock.unlock()
+        routes_to_draw_lock.unlock()
+        
+//        let reg_storyboard = UIStoryboard(name: "Login", bundle: nil)
+//        let reg_controller = reg_storyboard.instantiateViewController(withIdentifier: "Login") as! LoginController
+//        reg_controller.modalPresentationStyle = .fullScreen
+//        self.present(reg_controller, animated: true)
+        
+        self.dismiss(animated: true, completion: nil)
+
+//        UIApplication.shared.windows.first?.rootViewController = reg_controller
+//        UIApplication.shared.windows.first?.makeKeyAndVisible()
     }
     
 }
