edfa0bd50795a954b2cbb8b51eeef4d8e3f57074
Leonid_Faktorovich <lefaktorovich@edu.hse.ru>
2023-02-15 00:12:29 +0300

    add button for adding friend

--- DriverHelper/DriverHelper/FriendsController.swift
+++ DriverHelper/DriverHelper/FriendsController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class FriendsController: UIViewController {
     
+    @IBOutlet weak var add_friend_but: UIButton!
+    @IBOutlet weak var friend_login: UITextField!
     override func loadView() {
         super.loadView()
     }
@@ -10,6 +12,12 @@ class FriendsController: UIViewController {
         super.viewDidLoad()
     }
 
-
+    @IBAction func AddFriend(_ sender: Any) {
+        if (friend_login.text != nil && !friend_login.text!.isEmpty) {
+            main_user!.AddFriend(friend_login: friend_login.text!)
+            friend_login.text = ""
+        }
+    }
+    
 }
 
